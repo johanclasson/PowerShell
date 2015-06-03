a704c4406e4dbe7981ebc47929d1123119c98b14
johanclasson <classon@kth.se>
2015-06-03 22:16:06 +0200

    HyperV

---
+++ HyperV/HyperV.Tests.ps1
@@ -0,0 +1,244 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "HyperV" {
+    Context "Get-VMIP" {
+        Mock Get-VM { return [PSCustomObject]@{ NetworkAdapters = [PSCustomObject]@{ IPAddresses="169.254.188.87" },[PSCustomObject]@{ IPAddresses="169.254.188.99" } } }
+        $ip = Get-VMIP win2012r2_tmpl
+        It "returns the expected value" {
+            $ip | Should Be 169.254.188.87
+        }
+    }
+
+    $root = "TestDrive:\Hyper-V"
+    Context "New-VMFromConfig" {
+        $config = [xml]@"
+<config>
+	<VMSwitches>
+		<InternalVMSwitch name="LocalComputer" />
+		<InternalVMSwitch name="LocalComputer2" />
+		<ExternalVMSwitch name="ExternalEthernet" netAdapterName="Ethernet" />
+	</VMSwitches>
+	<VMs root="$root">
+		<VM name="BrandNewVM" switches="LocalComputer,ExternalEthernet" startupBytes="512MB" dynamicMemory="true" processorCount="4">
+			<DvdDrive path="TestDrive:\iso\dvd.iso" />
+			<DvdDrive path="TestDrive:\iso\dvd2.iso" />
+			<Vhd size="100GB" />
+			<Vhd filename="Extra stuff" size="7GB" />
+		</VM>
+		<VM name="NewVMFromTemplate" switches="LocalComputer,ExternalEthernet" startupBytes="1GB" dynamicMemory="false" processorCount="2">
+			<DvdDrive path="TestDrive:\iso\dvd.iso" />
+			<CopyVhd path="TestDrive:\Templates\Template.vhdx">
+                <ReplaceContent pathRelativeRoot="Windows\Panther\unattended.xml">
+                    <add key="__Locale__" value="en-US" />
+                    <add key="__ComputerName__" value="*" />
+                    <add key="__ProductKey__" value="AAAAA-BBBBB-CCCCC-DDDDD-EEEEE" />
+                    <add key="__Password__" value="p@ssword" />
+                </ReplaceContent>
+			</CopyVhd>
+            <MoveVhd filename="Moved disk" path="TestDrive:\Templates\Copies\Templ*.vhdx" />
+		</VM>
+		<VM name="VMWithoutDisks" />
+        <VM name="AlreadyPresentVM" />
+        <VM name="VMWithAlreadyPresentDisks">
+            <Vhd filename="AlreadyPresetDisk1" size="17GB" />
+            <CopyVhd filename="AlreadyPresetDisk2" path="TestDrive:\Templates\Template.vhdx" />
+            <MoveVhd filename="AlreadyPresetDisk3" path="TestDrive:\Templates\Copies\Templ*.vhdx" />
+        </VM>
+	</VMs>
+</config>
+"@
+        Mock New-VMSwitch { }
+        Mock Get-VMSwitch { return $true } -ParameterFilter { $Name -eq "LocalComputer2" }
+        Mock Get-VMSwitch { return $false }
+        Mock Get-VM { return $false } -ParameterFilter { $Name -eq "BrandNewVM" }
+        Mock Get-VM { return $false } -ParameterFilter { $Name -eq "NewVMFromTemplate" }
+        Mock Get-VM { return $false } -ParameterFilter { $Name -eq "VMWithoutDisks" }
+        Mock Get-VM { return $false } -ParameterFilter { $Name -eq "VMWithAlreadyPresentDisks" }
+        Mock Get-VM { return $true }
+        Mock New-VM {}
+        Mock Add-VMNetworkAdapter {}
+        Mock Set-VMMemory {}
+        Mock Set-VMProcessor {}
+        Mock Add-VMDvdDrive {}
+        Mock Add-VMNetworkAdapter {}
+        Mock New-VHD {}
+        Mock Add-VMHardDiskDrive { }
+        Mock Mount-VhdxAndGetLargestDriveLetter { return "TestDrive" }
+        Mock Dismount-DiskImage {}
+        # Yes, I'm cheating by not testing this. But I was not able to get the pipelining between the Mocked Get- and Remove-* to work.
+        Mock Get-VMDvdDrive { return @() }
+        Mock Remove-VMDvdDrive {}
+        Mock Get-VMNetworkAdapter { return @() }
+        Mock Remove-VMNetworkAdapter {}
+        # Done with cheating...
+        New-Item TestDrive:\Templates -ItemType Dir | Out-Null
+        "Template Content" | Out-File TestDrive:\Templates\Template.vhdx
+        New-Item TestDrive:\Templates\Copies -ItemType Dir | Out-Null
+        "Template Content" | Out-File TestDrive:\Templates\Copies\Template01.vhdx
+        "Template Content" | Out-File TestDrive:\Templates\Copies\Template02.vhdx
+        New-Item $root\VMWithAlreadyPresentDisks -ItemType Dir | Out-Null
+        "Already Present Content" | Out-File $root\VMWithAlreadyPresentDisks\AlreadyPresetDisk1.vhdx
+        "Already Present Content" | Out-File $root\VMWithAlreadyPresentDisks\AlreadyPresetDisk2.vhdx
+        "Already Present Content" | Out-File $root\VMWithAlreadyPresentDisks\AlreadyPresetDisk3.vhdx
+        New-Item TestDrive:\Windows\Panther -ItemType Dir | Out-Null
+        @"
+__Locale__
+__ComputerName__
+__ProductKey__
+__Password__
+"@ | Out-File TestDrive:\Windows\Panther\unattended.xml
+        New-VMFromConfig -Config $config -Verbose
+
+        It "creates an internal switch" {
+            Assert-MockCalled New-VMSwitch -ParameterFilter { $Name -eq "LocalComputer" -and $SwitchType -eq "Internal" }
+        }
+        It "creates an external switch" {
+            Assert-MockCalled New-VMSwitch -ParameterFilter { $Name -eq "ExternalEthernet" -and $NetAdapterName -eq "Ethernet" }
+        }
+        It "creates only switches wich does not exist already" {
+            Assert-MockCalled New-VMSwitch -Times 2 -Exactly 
+        }
+        It "creates VMs with new vhds" {
+            Assert-MockCalled New-VM -ParameterFilter { $Name -eq "BrandNewVM" -and 
+                                                        $Path -eq $root }
+        }
+        It "mounts the DVDs" {
+            Assert-MockCalled Add-VMDvdDrive -ParameterFilter { $VMName -eq "BrandNewVM" -and 
+                                                                $Path -eq "TestDrive:\iso\dvd.iso" }
+            Assert-MockCalled Add-VMDvdDrive -ParameterFilter { $VMName -eq "BrandNewVM" -and 
+                                                                $Path -eq "TestDrive:\iso\dvd2.iso" }
+        }
+        It "sets the processor count" {
+            Assert-MockCalled Set-VMProcessor -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                                 $Count -eq 4 }
+        }
+        It "sets memory configuration" {
+            Assert-MockCalled Set-VMMemory -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                              $StartupBytes -eq "512MB"}
+            Assert-MockCalled Set-VMMemory -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                              $DynamicMemoryEnabled -eq $true }
+        }
+        It "sets switches" {
+            Assert-MockCalled Add-VMNetworkAdapter -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                                      $SwitchName -eq  "LocalComputer" }
+            Assert-MockCalled Add-VMNetworkAdapter -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                                      $SwitchName -eq  "ExternalEthernet" }
+        }
+        It "adds VHDs" {
+            Assert-MockCalled New-VHD -ParameterFilter { $Path -eq "$root\BrandNewVM\BrandNewVM.vhdx" -and
+                                                         $SizeBytes -eq "100GB" }
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                                     $Path -eq "$root\BrandNewVM\BrandNewVM.vhdx" }
+        }
+        It "adds VHDs with custom name" {
+            Assert-MockCalled New-VHD -ParameterFilter { $Path -eq "$root\BrandNewVM\Extra stuff.vhdx" -and
+                                                         $SizeBytes -eq "7GB" }
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "BrandNewVM" -and
+                                                                     $Path -eq "$root\BrandNewVM\Extra stuff.vhdx" }
+        }
+        It "adds copies of template vhds" {
+            $copiedVhdxPath = "$root\NewVMFromTemplate\NewVMFromTemplate.vhdx"
+            $copiedVhdxPath | Should Exist
+            Get-Content $copiedVhdxPath | Should Be "Template Content"
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "NewVMFromTemplate" -and
+                                                                     $Path -eq $copiedVhdxPath }
+        }
+        It "replace stuff in unnatend.xml" {
+            $expectedContent = @"
+en-US
+*
+AAAAA-BBBBB-CCCCC-DDDDD-EEEEE
+p@ssword
+"@ -replace "`n","`r`n" # Sigh. Why do I have to change line endings?
+            $actualContent = Get-Content TestDrive:\Windows\Panther\unattended.xml -Raw
+            $actualContent.Trim() | Should Be $expectedContent
+            $copiedVhdxPath = "$root\NewVMFromTemplate\NewVMFromTemplate.vhdx"            
+            Assert-MockCalled Mount-VhdxAndGetLargestDriveLetter -ParameterFilter { $Path -eq $copiedVhdxPath }
+            Assert-MockCalled Dismount-DiskImage -ParameterFilter { $ImagePath -eq $copiedVhdxPath }
+        }
+        It "moves template vhds with whildcard" {
+            $movedVhdxPath = "$root\NewVMFromTemplate\Moved disk.vhdx"
+            $movedVhdxPath | Should Exist
+            Get-Content $movedVhdxPath | Should Be "Template Content"
+            Assert-MockCalled Add-VMHardDiskDrive -Exactly -Times 1 -ParameterFilter { $VMName -eq "NewVMFromTemplate" -and
+                                                                     $Path -eq $movedVhdxPath }
+            "TestDrive:\Templates\Copies\Template01.vhdx" | Should Not Exist
+            "TestDrive:\Templates\Copies\Template02.vhdx" | Should Exist
+        }
+        It "does not create VMs that are already there" {
+            Assert-MockCalled New-VM -Exactly -Times 0 { $Name -eq "AlreadyPresentVM" }
+        }
+        It "does not create VHDs that are already there" {
+            Assert-MockCalled New-VHD -Exactly -Times 0 -ParameterFilter { $Path -eq "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk1.vhdx" }
+            Get-Content "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk1.vhdx" | Should Be "Already Present Content"
+            Get-Content "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk2.vhdx" | Should Be "Already Present Content"
+            Get-Content "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk3.vhdx" | Should Be "Already Present Content"
+        }
+        It "adds VHDs that are already present on disk" {
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" -and
+                                                                     $Path -eq "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk1.vhdx" }
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" -and
+                                                                     $Path -eq "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk2.vhdx" }
+            Assert-MockCalled Add-VMHardDiskDrive -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" -and
+                                                                     $Path -eq "$root\VMWithAlreadyPresentDisks\AlreadyPresetDisk3.vhdx" }
+        }
+        It "use resonable default values for non mandatory attributes which are not set" {
+            Assert-MockCalled Set-VMMemory -Exactly -Times 0 -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" }
+            Assert-MockCalled Set-VMProcessor -Exactly -Times 0 -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" }
+            Assert-MockCalled Add-VMNetworkAdapter -Exactly -Times 0 -ParameterFilter { $VMName -eq "VMWithAlreadyPresentDisks" }
+        }
+    }
+
+    Context "New-VMFromConfig with bad configuration" {
+        Mock Get-VM { return $false }
+        Mock New-VM {}
+        # Yes, I'm cheating by not testing this. But I was not able to get the pipelining between the Mocked Get- and Remove-* to work.
+        Mock Get-VMDvdDrive { return @() }
+        Mock Remove-VMDvdDrive {}
+        Mock Get-VMNetworkAdapter { return @() }
+        Mock Remove-VMNetworkAdapter {}
+        # Done with cheating...
+
+        It "checks if root path does not exist" {
+            $config = [xml]@"
+<config>
+	<VMs root="TestDrive:\NotExistingFolder">
+	</VMs>
+</config>
+"@
+            { New-VMFromConfig -Config $config -Verbose } | Should Throw "Cannot find root folder path 'TestDrive:\NotExistingFolder' because it does not exist."
+        }
+        It "checks if VHD path does not exist" {
+            $config = [xml]@"
+<config>
+	<VMs root="TestDrive:\">
+         <VM name="DummyVM">
+            <CopyVhd filename="MissingDisk" path="TestDrive:\Templates\MissingTemplate.vhdx" />
+        </VM>
+	</VMs>
+</config>
+"@
+            { New-VMFromConfig -Config $config -Verbose } | Should Throw "Cannot find VHD path 'TestDrive:\Templates\MissingTemplate.vhdx' because it does not exist."
+        }
+        It "Does not throw with empty config" {
+            $config = [xml]@"
+<config>
+</config>
+"@
+            New-VMFromConfig -Config $config -Verbose
+        }
+        It "does not throw with almost empty config" {
+            $config = [xml]@"
+<config>
+	<VMSwitches>
+	</VMSwitches>
+	<VMs root="TestDrive:\">
+	</VMs>
+</config>
+"@
+            New-VMFromConfig -Config $config -Verbose
+        }
+    }
+}
---
+++ HyperV/HyperV.ps1
@@ -0,0 +1,185 @@
+﻿#Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+function Get-VMIP {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Name
+    )
+    return Get-VM -Name $Name | select -ExpandProperty NetworkAdapters | select -ExpandProperty IPAddresses | select -First 1 
+}
+
+function Mount-VhdxAndGetLargestDriveLetter {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    $old = Get-Volume
+    Mount-DiskImage $Path
+    $new = Get-Volume
+    return Compare-Object $old $new |
+        %{ $_.InputObject } | sort -Property Size -Descending |
+        select -First 1 -ExpandProperty DriveLetter
+}
+
+function New-InternalVMSwitchFromXml {
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]$Name
+    )
+    Process {
+        if (-not (Get-VMSwitch -Name $Name -ErrorAction SilentlyContinue)) {
+            New-VMSwitch -Name $Name -SwitchType Internal
+            Write-Verbose "Created internal switch: $Name"
+        }
+    }
+}
+
+function New-ExternalVMSwitchFromXml {
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]$Name,
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+        [string]$NetAdapterName
+    )
+    Process {
+        if (-not (Get-VMSwitch -Name $Name -ErrorAction SilentlyContinue)) {
+            New-VMSwitch -Name $Name -NetAdapterName $NetAdapterName
+            Write-Verbose "Created external switch: $Name - $NetAdapterName"
+        }
+    }
+}
+
+function New-VMFromXml {
+    param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [System.Xml.XmlElement]$Xml,
+        [Parameter(Mandatory=$true)]
+        [string]$Root
+    )
+    Process {
+        $name = $Xml.Name
+        if (Get-VM -Name $name -ErrorAction SilentlyContinue) {
+            return
+        }
+        New-VM -Name $name -Path $Root
+        Get-VMDvdDrive -VMName $name | Remove-VMDvdDrive
+        Get-VMNetworkAdapter -VMName $name | Remove-VMNetworkAdapter
+        Write-Verbose "Created VM: $name"
+        # Disks
+        $disks = $Xml.ChildNodes | ?{ $_.name -match "vhd" }
+        if ($disks) {
+            $disks | %{
+                $filename = $_.filename
+                if ([string]::IsNullOrEmpty($filename)) {
+                    $filename = $name
+                }
+                $path = "$Root\$name\$filename.vhdx"
+                if (Test-Path $path) {
+                    Write-Verbose "Skipped creating $path because it already existed" 
+                }
+                else {
+                    if ($_.Name -eq "Vhd") {
+                        $size = $_.size
+                        $expression = "New-VHD -Path ""$path"" -SizeBytes $size"
+                        Invoke-Expression $expression
+                        Write-Verbose "Created VHDX: $path - $size"
+                    }
+                    if ($_.Name -eq "CopyVhd" -or $_.Name -eq "MoveVhd") {
+                        $sourcePath = $_.path
+                        if (-not (Test-Path $sourcePath)) {
+                            throw "Cannot find VHD path '$sourcePath' because it does not exist."
+                        }
+                        $sourcePath = Resolve-Path $sourcePath | select -First 1
+                        New-Item (Split-Path $path -Parent) -ItemType Dir -ErrorAction SilentlyContinue | Out-Null
+                        if ($_.Name -eq "CopyVhd") {
+                            Write-Verbose "Starting to copy $sourcePath to $path..."
+                            Copy-Item $sourcePath $path -Force
+                        }
+                        else {
+                            Write-Verbose "Starting to move $sourcePath to $path..."
+                            Move-Item $sourcePath $path -Force
+                        }
+                        Write-Verbose "Done"
+                        $replaceContent = $_.ReplaceContent
+                        if ($replaceContent -and $replaceContent.add) {
+                            $drive = Mount-VhdxAndGetLargestDriveLetter $path
+                            $filePath = '{0}:\{1}' -f $drive,$replaceContent.pathRelativeRoot
+                            $expression = "(Get-Content ""$filePath"") "
+                            $replaceContent.add | %{
+                                $expression += "-replace '$($_.key)','$($_.value)' "
+                            }
+                            $expression += "| Out-File ""$filePath"""
+                            Invoke-Expression $expression
+                            Write-Verbose "Replaced content of $filePath"
+                            Dismount-DiskImage -ImagePath $path
+
+                        }
+                    }
+                }
+                Add-VMHardDiskDrive -VMName $name -Path $path
+                Write-Verbose "Added VHDX to VM: $path - $name"
+            }
+        }
+        # Switches
+        $switches = $Xml.switches | ?{ $_ -ne $null } | %{ $_.Split((',',' '), [System.StringSplitOptions]::RemoveEmptyEntries) }
+        if ($switches) {
+            $switches | %{
+                Add-VMNetworkAdapter -VMName $name -SwitchName $_
+                Write-Verbose "Added network adapter to VM: $_ - $name"
+            }
+        }
+        # DVDs
+        $dvds = $Xml.ChildNodes | ?{ $_.name -match "dvd" }
+        if ($dvds) {
+            $dvds | %{
+                $path = $_.path
+                Add-VMDvdDrive -VMName $name -Path $path
+                Write-Verbose "Added DVD to VM: $path - $name"
+            }
+        }
+        # Processor
+        $processorCount = $Xml.processorCount
+        if ($processorCount) {          
+            Set-VMProcessor -VMName $name -Count $processorCount
+            Write-Verbose "Set processor count to VM: $processorCount - $name"
+        }
+        # Memory
+        $startupBytes = $Xml.startupBytes
+        if ($startupBytes) {
+            $expression = 'Set-VMMemory -VMName "{0}" -StartupBytes {1}' -f $name,$startupBytes
+            Invoke-Expression $expression
+            Write-Verbose "Set startup bytes: $startupBytes - $name"
+        }
+        $dynamicMemory = $Xml.dynamicMemory
+        if ($dynamicMemory) {
+            $expression = 'Set-VMMemory -VMName "{0}" -DynamicMemoryEnabled ${1}' -f $name,$dynamicMemory
+            Invoke-Expression $expression
+            Write-Verbose "Set dynamic memory: $dynamicMemory - $name"
+        }
+    }
+}
+
+function New-VMFromConfig {
+    [CmdletBinding()]
+    param(
+        [string]$Path,
+        [xml]$Config
+    )
+    if (-not [string]::IsNullOrEmpty($Path)) {
+        $Config = [xml](Get-Content $Path)
+    }
+    $internalVmSwitches = $Config.config.VMSwitches.InternalVMSwitch
+    if ($internalVmSwitches) { $internalVmSwitches | New-InternalVMSwitchFromXml }
+    $externalVmSwitches = $Config.config.VMSwitches.ExternalVMSwitch
+    if ($externalVmSwitches) { $externalVmSwitches | New-ExternalVMSwitchFromXml }
+    if (-not $Config.config.VMs) {
+        return        
+    }
+    $root = $Config.config.VMs.root
+    if (-not (Test-Path $root)) {
+        throw "Cannot find root folder path '$root' because it does not exist."
+    }
+    $vms = $Config.config.VMs.VM
+    if ($vms) { $vms | New-VMFromXml -Root $root }
+}
